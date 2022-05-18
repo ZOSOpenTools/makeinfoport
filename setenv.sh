328c9e08eb445bf744534b8f35286a9f3fdf133f
Mike Fulton <mikefultonpersonal@gmail.com>
2022-05-17 20:25:20 -0400

    override 'bootstrap' command to be autogen.sh for 'git'

--- setenv.sh
+++ setenv.sh
@@ -12,8 +12,12 @@ export PORT_TYPE="TARBALL"
 export PORT_TARBALL_URL="https://ftp.gnu.org/gnu/texinfo/texinfo-6.8.tar.gz"
 export PORT_TARBALL_DEPS="curl gzip make m4 perl autoconf"
 
-export PORT_GIT_URL="https://www.gnu.org/s/texinfo/texinfo.git"
-export PORT_GIT_DEPS="git make m4 perl autoconf automake xz"
+export PORT_GIT_URL="https://git.savannah.gnu.org/git/texinfo.git"
+export PORT_GIT_DEPS="git make m4 perl autoconf libtool automake xz"
 
 export PORT_EXTRA_CFLAGS=""
 export PORT_EXTRA_LDFLAGS=""
+
+if [ "${PORT_TYPE}x" = "GITx" ]; then
+	export PORT_BOOTSTRAP="./autogen.sh"
+fi

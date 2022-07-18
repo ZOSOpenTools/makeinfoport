272a901c8730e28809f4120856db1c98c3456d2e
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-18 09:41:37 -0400

    Add polling for changes in the jenkins script

--- cicd.groovy
+++ cicd.groovy
@@ -1,5 +1,23 @@
 node('linux') 
 {
+        stage ('Poll') {
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/makeinfoport.git']]])
+                ])
+
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/utils.git']]])
+                ])
+        }
+
         stage('Build') {
                 build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'makeinfoport'), string(name: 'DESCRIPTION', 'makeinfo is a program that converts a Texinfo file into an Info file, HTML file, or plain text.' )]
         }

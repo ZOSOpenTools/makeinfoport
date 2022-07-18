445c3ccd75042525bf66e26069c06a0b216960ac
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-18 08:47:00 -0400

    Add description to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux') 
 {
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'makeinfoport'), string(name: 'DESCRIPTION', 'makeinfoport' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'makeinfoport'), string(name: 'DESCRIPTION', 'makeinfo is a program that converts a Texinfo file into an Info file, HTML file, or plain text.' )]
         }
 }

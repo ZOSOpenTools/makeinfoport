8f725de2e1e16d50104e72d75aaef50999fe625e
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-20 09:55:08 -0400

    Remove polling of utils.git

--- cicd.groovy
+++ cicd.groovy
@@ -10,6 +10,6 @@ node('linux')
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'makeinfoport'), string(name: 'DESCRIPTION', value: 'makeinfo is a program that converts a Texinfo file into an Info file, HTML file, or plain text.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'makeinfoport'), string(name: 'PORT_DESCRIPTION', value: 'makeinfo is a program that converts a Texinfo file into an Info file, HTML file, or plain text.' )]
         }
 }
